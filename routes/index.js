e1440691ff6af5d90de42f9364828abba97cb66a
石木洪 <13920096993@126.com>
2020-11-27 11:14:49 +0800

    update: sql add

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,34 @@ var mysql = require('mysql');
 var router = express.Router();
 
 // 创建连接mysql配置
-// var connection = mysql.createConnection({
-//   host: '127.0.0.1', //数据库地址
-//   port: '3306',//端口号
-//   user: 'root',//用户名
-//   password: '123456',//密码
-//   database: 'navicatMysql'//数据库名称
-// });
+var connection = mysql.createConnection({
+  host: '127.0.0.1', //数据库地址
+  port: '3306',//端口号
+  user: 'root',//用户名
+  password: '12345678',//密码
+  database: 'shimh'//数据库名称
+});
 // // 建立连接
-// connection.connect();
+connection.connect();
 
-// var sql = 'SELECT * FROM `db`';
-// var rsData = '';
-// // setTimeout(()=> {
-//   console.log('111111');
-//   connection.query(sql, function(err, result) {
-//     if (err) {
-//         console.log('[SELECT ERROR]：', err.message);
-//     }
-//     rsData = JSON.stringify(result)
-//     console.log('==', rsData);
-//     console.log(result);
-//   })
-// // }, 3000)
+var sql = 'select * from runoob_tbl';
+var rsData = '';
+  
 
 // console.log('===', rsData)
 /* GET home page. */
 router.get('/api1', function(req, res, next) {
   // res.render('index', { title: 'Express11' });
-  console.log('==api1 req:', req.query)
-  res.send({data: req.query});
+  connection.query(sql, function(err, result) {
+    if (err) {
+      console.log('[SELECT ERROR]：', err.message);
+    }
+    rsData = JSON.stringify(result)
+    console.log('==', rsData);
+    console.log(result);
+    res.send({data: result});
+  })
+
 });
 router.get('/api2', function(req, res, next) {
   res.send('respond with a resource222');
